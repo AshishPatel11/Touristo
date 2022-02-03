b37f88e71b45b51894a16587cfcd21de531716c7
Ashish <ashishpatel287680@gmail.com>
2022-02-04 00:36:30 +0530

    animation done by ashish

--- js/signup.js
+++ js/signup.js
@@ -18,9 +18,100 @@ const navSlide = () => {
     });
 }
 navSlide();
-// ...............................................👇animation👇............................................
+// ...............................................👇animation👇.................................................
 let tl = gsap.timeline({
     scrollTrigger: {
+        trigger: '.sec',
+        start: '0%',
+        end: '100%',
+        scrub: 1,
+    }
+});
+let tl2 = gsap.timeline({
+    scrollTrigger: {
+        trigger: '.sec',
+        start: '0%',
+        end: '100%',
+        scrub: 1,
+    }
+});
+let tl4 = gsap.timeline({
+    scrollTrigger: {
+        trigger: '.sec',
+        start: '0%',
+        end: '100%',
+        scrub: 1,
+    }
+});
+let tl5 = gsap.timeline({
+    scrollTrigger: {
+        trigger: '.sec',
+        start: '0%',
+        end: '100%',
+        scrub: 1,
+    }
+});
+let tl6 = gsap.timeline({
+    scrollTrigger: {
+        trigger: '.sec',
+        start: '0%',
+        end: '100%',
+        scrub: 1,
+    }
+});
+let tl7 = gsap.timeline({
+    scrollTrigger: {
+        trigger: '.sec',
+        start: '0%',
+        end: '100%',
+        scrub: 1,
+    }
+});
+let tl8 = gsap.timeline({
+    scrollTrigger: {
+        trigger: '.sec',
+        start: '25%',
+        end: '80%',
+        scrub: 1,
+    }
+});
+let tl9 = gsap.timeline({
+    scrollTrigger: {
+        trigger: '.nav-container',
+        start: '0%',
+        end: '1%',
+        scrub: 1,
+    }
+});
+let tl10 = gsap.timeline({
+    scrollTrigger: {
+        trigger: '.nav-container',
+        start: '0%',
+        end: '1%',
+        scrub: 1,
+    }
+});
+let tl3 = gsap.timeline({
+    scrollTrigger: {
+        trigger: '.nav-container',
+        start: '0%',
+        end: '5000%',
+        scrub: 1,
+        pin: true,
+        pinSpacing: false,
 
     }
+});
+tl.to(".intro-text", { y: -70 });
+tl2.to(".tour-svg", { y: -70 });
+tl4.to(".wave-img", { y: 50 });
+tl5.fromTo("#girl", { opacity: 1 }, { opacity: 0, x: 70 });
+tl6.fromTo("#plane", { x: 40, y: 10 }, { x: -500, y: -50 });
+tl7.to("#sun", { y: 60 });
+tl8.fromTo(".form-container", { x: -80, opacity: 0 }, { x: 0, opacity: 1 });
+tl9.to(".nav-container", {
+    backgroundColor: "#332f2f", color: "rgb(255, 255, 255)"
+});
+tl10.to(".nav-option a", {
+    color: "rgb(255, 255, 255)"
 });
