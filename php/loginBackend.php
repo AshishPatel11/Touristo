7c69a85e4137426590db6b327eb02d6296e0381b
Dixit <varadixit@gmail.com>
2022-03-06 13:29:14 +0530

    hi

--- php/loginBackend.php
+++ php/loginBackend.php
@@ -8,18 +8,20 @@ if ($_SERVER['REQUEST_METHOD'] == "POST") {
     $result = mysqli_query($conn, $select);
     $data = mysqli_fetch_assoc($result);
     $count = mysqli_num_rows($result);
-
+    
     if ($count == 1 and password_verify($_POST['passwd'],$data['passwd']))
     {
+        session_start();
+        $_SESSION['uname'] = $data['uname'];
         echo "
             <script>
-                alert(`Login Successful!! Welcome $data[uname]`);
-                location.replace('home.php');
+                alert(`Login Successful!! Welcome $_SESSION[uname]`);
+                location.replace('main.php');
             </script>";
     } else {
         echo "
             <script>
-                alert(`Invalid Credentials, $conn->error`);
+                alert(`Invalid Credentials`);
             </script>";
     }
 }
