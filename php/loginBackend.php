828b660512f7ec7fa09f40431a29989dfc1de02b
Dixit <varadixit@gmail.com>
2022-03-06 12:27:54 +0530

    hi

--- php/loginBackend.php
+++ php/loginBackend.php
@@ -5,10 +5,10 @@ if ($_SERVER['REQUEST_METHOD'] == "POST") {
 
     $select = "SELECT * FROM `user_tbl` where emailid='$_POST[email]';";
 
-    $result = $conn->query($select);
+    $result = mysqli_query($conn, $select);
     $data = mysqli_fetch_assoc($result);
     $count = mysqli_num_rows($result);
-    
+
     if ($count == 1 and password_verify($_POST['passwd'],$data['passwd']))
     {
         echo "
