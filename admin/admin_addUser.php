6c7ccd2f07514670b2cc1809ef550f1b0ae93512
Dixit <varadixit@gmail.com>
2022-03-16 18:31:55 +0530

    hi

--- admin/admin_addUser.php
+++ admin/admin_addUser.php
@@ -1,16 +1,42 @@
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="css/admin_addUser.css">
     <link rel="shortcut icon" href="../css/images/svg/title.svg">
     <link rel="stylesheet" href="./css/admin_nav.css">
     <!-- <link rel="stylesheet" href="./css/admin_home.css"> -->
     <title>Add User</title>
 </head>
+
 <body>
     <?php include_once "admin_nav.php";
     ?>
+
+    <div class="admin-container">
+        <form action="admin_addUser.php" method="post">
+            <h1>Add User</h1>
+            <div class="input-box">
+                <label for="aname">Name</label>
+                <input type="text" id="aname" name="aname" required>
+            </div>
+            <div class="input-box">
+                <label for="email">Email</label>
+                <input type="email" id="email" name="email" required>
+            </div>
+            <div class="input-box">
+                <label for="contact">Phone no.</label>
+                <input type="text" id="contact" name="contact" required>
+            </div>
+            <div class="input-box">
+                <label for="password">Password</label>
+                <input type="password" id="password" name="password" required>
+            </div>
+        </form>
+    </div>
 </body>
+
 </html>
