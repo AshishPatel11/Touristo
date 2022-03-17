03efd18b295e97fa3b420b02848768956886da57
Dixit <varadixit@gmail.com>
2022-03-17 12:47:39 +0530

    hi

--- admin/admin_addUser.php
+++ admin/admin_addUser.php
@@ -1,3 +1,77 @@
+<?php
+
+include "../php/connection.php";
+
+// error variable declaration for printing error
+$unameerr = $emailerr = $contacterr = $passworderr = $addresserr = "";
+
+if ($_SERVER['REQUEST_METHOD'] == 'POST') {
+
+    // Assigning values to the variables
+    $uid = rand(100000, 1000000);
+    $uname = mysqli_real_escape_string($conn, $_POST['uname']);
+    $email = mysqli_real_escape_string($conn, $_POST['email']);
+    $contact = mysqli_real_escape_string($conn, $_POST['contact']);
+    $password = mysqli_real_escape_string($conn, $_POST['password']);
+    $cpassword = mysqli_real_escape_string($conn, $_POST['cpassword']);
+    $address = mysqli_real_escape_string($conn, $_POST['address']);
+
+    // Password hashing
+    $hash = password_hash($password, PASSWORD_BCRYPT);
+
+    // Email fetching
+    $emailquery = "SELECT * FROM admin_tbl WHERE emailid='$email'";
+    $eresult = mysqli_query($conn, $emailquery);
+    $ecount = mysqli_num_rows($eresult);
+
+    // Phone no fetching
+    $contactquery = "SELECT * FROM admin_tbl WHERE phno='$contact'";
+    $cresult = mysqli_query($conn, $contactquery);
+    $ccount = mysqli_num_rows($cresult);
+
+    // Main Validation
+    if (!preg_match("/^[a-zA-Z ]*$/", $uname)) {
+        $unameerr = "Only letter and space allowed!";
+    } else {
+        if ($ecount > 0) {
+            $emailerr = "Email already exist!";
+        } else {
+            if (!preg_match("/^[0-9]{10}+$/", $contact)) {
+                $contacterr = "Phone number must be 10 digits!";
+            } else {
+                if ($ccount > 0) {
+                    $contacterr = "Number already exist!";
+                } else {
+                    if ($password !== $cpassword) {
+                        $passworderr = "Password are not matching";
+                    } else {
+
+                        $insert = "INSERT INTO admin_tbl(`uid`, `uname`, `emailid`, `phno`, `passwd`, `adrs`) VALUES ('$uid','$uname','$email','$contact','$hash','$address')";
+
+                        $result = mysqli_query($conn, $insert);
+
+                        if($result){
+                            ?>
+                                <script>
+                                    alert(`User added!`);
+                                </script>
+                            <?php
+                        }
+                        else{
+                            echo 
+                                "<script>
+                                    alert(`User adding Failed $conn->error`);
+                                </script>";
+                        }
+                    }
+                }
+            }
+        }
+    }
+}
+
+?>
+
 <!DOCTYPE html>
 <html lang="en">
 
@@ -13,6 +87,7 @@
 </head>
 
 <body>
+
     <?php include_once "admin_nav.php";
     ?>
 
@@ -20,24 +95,33 @@
         <form action="admin_addUser.php" method="post">
             <h1>Add User</h1>
             <div class="input-box">
-                <label for="aname">Name</label>
-                <input type="text" id="aname" name="aname" required>
+                <label for="uname">Name</label>
+                <input type="text" id="uname" name="uname" required>
+                <?php echo "$unameerr"; ?>
             </div>
             <div class="input-box">
                 <label for="email">Email</label>
                 <input type="email" id="email" name="email" required>
+                <?php echo "$emailerr"; ?>
             </div>
             <div class="input-box">
                 <label for="contact">Phone no.</label>
                 <input type="text" id="contact" name="contact" required>
+                <?php echo "$contacterr"; ?>
             </div>
             <div class="input-box">
                 <label for="password">Password</label>
                 <input type="password" id="password" name="password" required>
             </div>
+            <div class="input-box">
+                <label for="cpassword">Confirm Password</label>
+                <input type="password" id="cpassword" name="cpassword" required>
+                <?php echo "$passworderr"; ?>
+            </div>
             <div class="input-area">
                 <label for="address">Address</label>
-                <textarea name="address" id="address" cols="30" rows="6"></textarea>
+                <textarea name="address" id="address" cols="30" rows="6" required></textarea>
+                <?php echo "$addresserr"; ?>
             </div>
             <div class="input-btn">
                 <input type="submit" value="Submit" name="submit">
