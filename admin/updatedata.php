a899c3f726961d80ac3b4714596a0695de848dc8
Dixit <varadixit@gmail.com>
2022-03-24 10:51:47 +0530

    hi

--- admin/updatedata.php
+++ admin/updatedata.php
@@ -13,12 +13,13 @@ $phno = $_GET['phno'];
 $ac = $_GET['acct'];
 
 if ($_SERVER['REQUEST_METHOD'] == 'POST') {
-    
+
+    $userid = $_POST['uid'];
     $nname = $_POST['uname'];
     $nemail = $_POST['email'];
     $nphno = $_POST['phno'];
     $nacctype = $_POST['acctype'];
-    
+
     // Main Validation
     if (!preg_match("/^[a-zA-Z ]*$/", $nname)) {
         $unameerr = "Only letter and space allowed!";
@@ -26,7 +27,7 @@ if ($_SERVER['REQUEST_METHOD'] == 'POST') {
         if (!preg_match("/^[0-9]{10}+$/", $nphno)) {
             $contacterr = "Phone number must be 10 digits!";
         } else {
-            $update = "UPDATE user_tbl SET `uname`='$nname',`emailid`='$nemail',`phno`='$nphno',`acc_typ`='$nacctype' WHERE uid=$uid";
+            $update = "UPDATE `user_tbl` SET uname='$nname',emailid='$nemail',phno='$nphno',acc_typ='$nacctype' WHERE uid='$userid'";
 
             $result = mysqli_query($conn, $update);
 
@@ -34,6 +35,7 @@ if ($_SERVER['REQUEST_METHOD'] == 'POST') {
 ?>
                 <script>
                     alert(`User data updated!`);
+                    location.replace('admin_updateUser.php');
                 </script>
 <?php
             } else {
@@ -59,6 +61,30 @@ if ($_SERVER['REQUEST_METHOD'] == 'POST') {
     <link rel="stylesheet" href="./css/admin_nav.css">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Update data</title>
+    <style>
+        table {
+            border-collapse: collapse;
+        }
+
+        table th,
+        table td {
+            padding: 10px;
+        }
+
+        table,
+        th,
+        td {
+            border: 1px solid #000;
+        }
+
+        table th {
+            background-color: #272b33;
+            color: #fff;
+            vertical-align: middle;
+            padding-left: 10px;
+            padding: 20px;
+        }
+    </style>
 </head>
 
 <body>
@@ -69,46 +95,65 @@ if ($_SERVER['REQUEST_METHOD'] == 'POST') {
             </div>
             <br>
             <div class="input">
-   
-                <?php echo $uid; ?><br>
+                <label for="uid">UserID</label>
+                <input type="text" name="uid" id="uid" required>
+            </div>
+            <div class="input">
                 <label for="uname">User name</label>
-                <input type="text" name="uname" id="uname" value="<?php echo $uname; ?>" required>
+                <input type="text" name="uname" id="uname" required>
                 <?php echo "$unameerr"; ?>
             </div>
             <div class="input">
                 <label for="email">Email</label>
-                <input required value="<?php echo $email; ?>" type="email" name="email" id="email">
+                <input required type="email" name="email" id="email">
                 <?php echo "$emailerr"; ?>
 
             </div>
             <div class="input">
                 <label for="phno">Phone no</label>
-                <input type="text" required name="phno" id="phno" value="<?php echo $phno; ?>">
+                <input type="text" required name="phno" id="phno">
                 <?php echo "$contacterr"; ?>
 
-            </div>
-            <div class="input">
-                <label for="acc">Old A/c type : </label>
-                <?php echo $ac; ?>
-            </div>
-            <div class="input">
-                <label for="acctype"> New A/c type</label>
+                <div class="input">
+                    <label for="acctype"> New A/c type</label>
 
-                <select name="acctype" id="acctype" id="" required>
-                    <option disabled selected>--Select A/C type--</option>
-                    <option value="user">User</option>
-                    <option value="admin">Admin</option>
-                    <option value="guide">Guide</option>
-                    <option value="hotel">Hotel</option>
-                </select>
-            </div>
-            <center>
+                    <select name="acctype" id="acctype" id="" required>
+                        <option disabled selected>--Select A/C type--</option>
+                        <option value="user">User</option>
+                        <option value="admin">Admin</option>
+                        <option value="guide">Guide</option>
+                        <option value="hotel">Hotel</option>
+                    </select>
+    </div>
+                    <br>
+                    <div class="submit">
+                        <input type="submit" value="Update" name="update">
+                    </div>
                 <br>
-                <div class="submit">
-                    <input type="submit" value="Update" name="update">
-                </div>
-            </center>
+                <div class="table">
+                    <h1>Old data</h1>
+                    <table>
+                        <thead>
+                            <tr>
+                                <th>UserID</th>
+                                <th>Name</th>
+                                <th>EmailID</th>
+                                <th>Phone no.</th>
+                                <th>A/c type</th>
+                            </tr>
+                        </thead>
+                        <tbody>
+                            <tr class='active-row'>
+                                <td><?php echo $uid; ?> </td>
+                                <td><?php echo $uname; ?> </td>
+                                <td><?php echo $email; ?> </td>
+                                <td><?php echo $phno; ?> </td>
+                                <td><?php echo $ac; ?> </td>
+                            </tr>
+                        </tbody>
+                    </table>
 
+                </div>
         </form>
     </div>
 </body>
