df8ee4714e9d4c1bafd6a3585d009f03a1c64519
Dixit <varadixit@gmail.com>
2022-04-04 16:34:02 +0530

    ggg

--- logout.php
+++ logout.php
@@ -1,12 +1,5 @@
 <?php
 session_start();
-if(isset($_SESSION['uname'])){
     session_destroy();
     echo "<script>  alert(`You have been logout!`);
                     location.replace(`login.php`); </script>";
-}
-else{
-    echo "<script>  alert(`Login first!`);
-    location.replace(`login.php`); </script>";
-}
-?>
