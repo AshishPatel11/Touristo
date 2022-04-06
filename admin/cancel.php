a03afc96f36d46209022092d1eed4157aae726ce
Dixit <varadixit@gmail.com>
2022-04-06 22:26:14 +0530

    hi

--- admin/cancel.php
+++ admin/cancel.php
@@ -4,13 +4,13 @@ include '../php/connection.php';
 
 $srno = $_GET['srno'];
 
-$update = " UPDATE book_tbl SET status = 'canceled' WHERE srno = '$srno' ";
+$update = " UPDATE book_tbl SET status = 'cancelled' WHERE srno = '$srno' ";
 $run = mysqli_query($conn, $update);
 
 if($run){
     ?>
     <script>
-        alert(`Booking canceled!`);
+        alert(`Booking cancelled!`);
         location.replace('manage_book.php');
     </script>
     <?php
